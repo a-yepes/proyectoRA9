e6505666c0c084a35778a704aaef6889e50b2160
a-yepes <anuski154@gmail.com>
2026-05-14 11:10:43 +0200

    Constructor, getters y setters de Reparacion y Cliente

--- src/main/java/pio/daw/proyectoRA9/models/Cliente.java
+++ src/main/java/pio/daw/proyectoRA9/models/Cliente.java
@@ -1,21 +1,25 @@
 package pio.daw.proyectoRA9.models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
+@NoArgsConstructor //me genera automaticamente el constructor vacio, por eso no lo añado despues.
 public class Cliente {
 
     @Id
@@ -26,5 +30,23 @@ public class Cliente {
     private String email;
     @Column(unique=true)
     private String matricula;
+
+    @OneToMany(mappedBy = "reparacion", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reparacion> reparaciones = new ArrayList<>();
+
+    public Cliente(Long id, String nombre, String telefono, String email, String matricula) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.matricula = matricula;
+       
+    }
+    
+
+    
+
+    
+
     
 }
--- src/main/java/pio/daw/proyectoRA9/models/Reparacion.java
+++ src/main/java/pio/daw/proyectoRA9/models/Reparacion.java
@@ -10,13 +10,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 public class Reparacion {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,8 +27,67 @@ public class Reparacion {
     private LocalDate fechaSalida;
     private Double costeEuros;
     private String estado;
-    
 
+    @ManyToOne
+    private Cliente cliente;
 
-    
+    // getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public Double getCosteEuros() {
+        return costeEuros;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    // setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFechaEntrada(LocalDate fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public void setCosteEuros(Double costeEuros) {
+        this.costeEuros = costeEuros;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
 }
